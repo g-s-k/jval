06356acd59d3ec219fa700e056e37de7557978fc
George Kaplan <george@georgekaplan.xyz>
2019-06-03 09:33:34 -0400

    add "go to error" feature

--- examples/www/src/static/validate.js
+++ examples/www/src/static/validate.js
@@ -1,10 +1,21 @@
 import { default as init, validate } from "./www.js";
 
+const emptyIndices = { start: null, end: null };
+
 (async function run() {
   await init("./www_bg.wasm");
 
+  let errorIndices = emptyIndices;
+
   const entryBox = document.getElementById("json-entry-box");
   const errorDisp = document.getElementById("error-display-box");
+  const gotoError = document.getElementById("goto-error-btn");
+
+  gotoError.addEventListener("click", function handleGotoClick() {
+    entryBox.selectionStart = errorIndices.start;
+    entryBox.selectionEnd = errorIndices.end;
+    entryBox.focus();
+  });
 
   entryBox.addEventListener("input", function handleChange({
     target: { value = "" }
@@ -16,9 +27,13 @@ import { default as init, validate } from "./www.js";
       const msg = "error between " + start + " and " + end + ".";
       errorDisp.innerText = msg;
       entryBox.setCustomValidity(msg);
+      gotoError.disabled = false;
+      errorIndices = { start, end };
     } else {
       errorDisp.innerText = "good!";
       entryBox.setCustomValidity("");
+      gotoError.disabled = true;
+      errorIndices = emptyIndices;
     }
   });
 })();
