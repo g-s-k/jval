a9b02fb2c43edc08b494fdc05b4fe6b60b8aac07
George Kaplan <george.kaplan@visionistinc.com>
2019-06-03 15:45:40 -0400

    i hate everything

--- examples/www/build.sh
+++ examples/www/build.sh
@@ -5,6 +5,6 @@ OUTDIR=$(realpath "$DIRNAME/../../target/pkg")
 
 wasm-pack build --target web --no-typescript --out-dir "$OUTDIR" "$DIRNAME"
 
-rm -v "$OUTDIR/package.json"
+rm -v "$OUTDIR/package.json" "$OUTDIR/.gitignore"
 
 cp -v "$DIRNAME"/src/static/* "$OUTDIR"
