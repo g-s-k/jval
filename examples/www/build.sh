8fc74c7f290484da78f3480fe1d1e99c23302d14
George Kaplan <george@georgekaplan.xyz>
2019-06-07 05:00:00 -0400

    add mac support to web scripts

--- examples/www/build.sh
+++ examples/www/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 DIRNAME=$(realpath $(dirname "$0"))
 OUTDIR=$(realpath "$DIRNAME/../../target/pkg")
 
--- examples/www/start.sh
+++ examples/www/start.sh
@@ -5,13 +5,11 @@
 mkdir -p ../../target/pkg && cd ../../target/pkg && python3 -m http.server ${1:-8000} &
 PYPID=$!
 
-trap 'kill $PYPID' 2
-
 if [ $(uname) = "Linux" ]; then
+    trap 'kill $PYPID' 2
     inotifywait -rme modify,close_write,move,create,delete ./src ../../src |
     while read events; do ./build.sh; done
 else
-    kill $PYPID
-    exit 1
+    trap 'pkill Python' 2
+    fswatch -o ./src ../../src | xargs -n1 -I{} ./build.sh
 fi
-
